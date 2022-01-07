8f647b33ec2b819a13ea470859373d6ced0ca22f
Holulosamo <eliasrodriguez000@gmail.com>
2022-01-07 04:08:20 -0300

    making the header

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-import './style.css'
+import './style.css';
+
+const navBtn = document.querySelector('.hamburger'),
+  navMenu = document.querySelector('.nav-menu'),
+  navLink = document.querySelectorAll('.nav-menu__link');
+
+navBtn.addEventListener('click', () =>{
+  navBtn.classList.contains("is-active")
+    ? navBtn.classList.remove("is-active")
+    : navBtn.classList.add("is-active");
+
+  navMenu.classList.contains("nav-menu-active")
+    ? navMenu.classList.remove("nav-menu-active")
+    : navMenu.classList.add("nav-menu-active");
+});
+
+navLink.forEach(navLink => {
+  navLink.addEventListener("click", () => {
+    if (navMenu.classList.contains("nav-menu-active") && navBtn.classList.contains("is-active")) 
+    {
+      navMenu.classList.remove("nav-menu-active");
+      navBtn.classList.remove("is-active");
+    }
+  });
+})
